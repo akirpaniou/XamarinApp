77c766868d1afe4fc62bcda7d827b5cbedd2bb03
Artsiom Kirpaniou <kirpaniou@gmail.com>
2022-04-26 01:25:59 +0300

    Add interface in Main Page App

---
+++ ElenStore/ElenStore/ElenStore/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ElenStore
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            StackLayout layout = new StackLayout();
+
+            Label label1 = new Label();
+            Label label2 = new Label();
+
+            label1.Text = "CodeLearn";
+            label1.TextColor = Color.Red;
+            label1.FontSize = 30;
+            label1.FontAttributes = FontAttributes.Bold;
+            label1.HorizontalOptions = LayoutOptions.Center;
+            label1.BackgroundColor = Color.Blue;
+            
+
+            label2.Text = "Learn a programming language";
+            label2.TextColor = Color.Green;
+            label2.FontSize = 20;
+            label2.FontAttributes = FontAttributes.Bold;
+            label2.HorizontalOptions = LayoutOptions.Center;
+
+
+            layout.Children.Add(label1);
+            layout.Children.Add(label2);
+
+            //Image image1 = new Image();
+            //image1.Source = ImageSource.FromResource("ElenStore.langMainPge.jpg");
+
+            Content = layout;
+
+            Frame imageFrame = new Frame
+            {
+                Content = new StackLayout
+                {
+
+                }
+            };
+
+        }
+
+    }
+}
---
+++ ElenStore/ElenStore/ElenStore/StartPage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ElenStore
+{
+    class StartPage : ContentPage
+    {
+        public StartPage()
+        {
+            Label header = new Label() { Text = "Привет из Xamarin Forms" };
+            this.Content = header;
+        }
+    }
+}
