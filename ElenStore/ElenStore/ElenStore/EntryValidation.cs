6f0dca8c1f29e90369478db5ab5fc2305ac36166
Artsiom Kirpaniou <kirpaniou@gmail.com>
2022-05-13 23:13:38 +0300

    Add class for triggers, working with pushotifications

---
+++ ElenStore/ElenStore/ElenStore/EntryValidation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ElenStore
+{
+    class EntryValidation : TriggerAction<Entry>
+    {
+        protected override void Invoke(Entry sender)
+        {
+            int number;
+            if (!Int32.TryParse(sender.Text, out number))
+                sender.BackgroundColor = Color.Red;
+            else
+                sender.BackgroundColor = Color.Default;
+        }
+    }
+}
